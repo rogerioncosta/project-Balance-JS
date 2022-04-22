ab0784bcb850a3ef70f941576fef1b5c7871605f
Rogério Costa <rogerioncosta1@gmail.com>
2022-04-22 18:33:08 -0300

    inserindo dados

--- scrips.js
+++ scrips.js
@@ -19,16 +19,55 @@ const caixaFormulario = {
         // }
 }
 
-const transacao = {
+
+const Transacao = {
+    all: [
+        // {
+        //     // id: 1,
+        //     descricao: 'Luz',
+        //     valor: -50000,
+        //     data: '23/01/2022'
+        // }, 
+        // {
+        //     // id: 2,
+        //     descricao: 'Website',
+        //     valor: 500000,
+        //     data: '24/01/2022'
+        // }, 
+        // {
+        //     // id: 3,
+        //     descricao: 'Internet',
+        //     valor: -20012,
+        //     data: '25/01/2022'
+        // },
+        // {
+        //     // id: 4,
+        //     descricao: 'App',
+        //     valor: 200000,
+        //     data: '25/01/2022'
+        // }
+    ],
+
+    add(transacao) {
+        Transacao.all.push(transacao)
+
+        app.reiniciar()
+    },
+
+    remover(indice) {
+        Transacao.all.splice(indice, 1)
+        app.reiniciar()
+    },
+
     entradas() {
         let entrada = 0
         // pegar todas as transações
         // para cada transação,
-        transacoes.forEach(transacoes => {
+        Transacao.all.forEach(transacao => {
             // se ela for maior que zero
-            if(transacoes.valor > 0) {
+            if(transacao.valor > 0) {
                 // somar a uma variavel e retornar ela
-                entrada = entrada + transacoes.valor
+                entrada = entrada + transacao.valor
             }
         })
         return entrada
@@ -38,11 +77,11 @@ const transacao = {
         let saida = 0
         // pegar todas as transações
         // para cada transação,
-        transacoes.forEach(transacoes => {
+        Transacao.all.forEach(transacao => {
             // se ela for menor que zero
-            if(transacoes.valor < 0) {
+            if(transacao.valor < 0) {
                 // somar a uma variavel e retornar ela
-                saida = saida + transacoes.valor
+                saida = saida + transacao.valor
             }
         })
         return saida
@@ -50,49 +89,22 @@ const transacao = {
 
     total() {
         // valor das entradas - as saídas
-        return transacao.entradas() + transacao.saidas()
+        return Transacao.entradas() + Transacao.saidas()
     }
 }
-
-const transacoes = [
-    {
-        id: 1,
-        descricao: 'Luz',
-        valor: -50000,
-        data: '23/01/2022'
-    }, 
-    {
-        id: 2,
-        descricao: 'Website',
-        valor: 500000,
-        data: '24/01/2022'
-    }, 
-    {
-        id: 3,
-        descricao: 'Internet',
-        valor: -20012,
-        data: '25/01/2022'
-    },
-    {
-        id: 4,
-        descricao: 'App',
-        valor: 200000,
-        data: '25/01/2022'
-    }
-]
 
 const DOM = {
     // pega as tags #dados-tabela tbody e coloca na var containerTransacoes
     containerTransacoes: document.querySelector('#dados-tabela tbody'),
 
     // Cria a tag tr
-    adicionarTransacao(transacoes, indice) {       
+    adicionarTransacao(transacao, indice) {       
 
-        console.log(transacoes)
+        console.log(transacao)
 
         const tr = document.createElement('tr')
         
-        tr.innerHTML = DOM.transacaoInnerHTML(transacoes)
+        tr.innerHTML = DOM.transacaoInnerHTML(transacao)
         // Dentro do html do tr, coloca os tds
         // a var tr tem o valor dos tds
 
@@ -103,15 +115,15 @@ const DOM = {
     },
 
     // Cria os tds para a tag tr
-    transacaoInnerHTML(transacoes) {
-        const CSSclasse = transacoes.valor > 0 ? "entrada" : "saida"
+    transacaoInnerHTML(transacao) {
+        const CSSclasse = transacao.valor > 0 ? "entrada" : "saida"
 
-        const valor =  uteis.formatoMoeda(transacoes.valor)
+        const valor =  uteis.formatoMoeda(transacao.valor)
 
         const html =
-        `   <td class="descricao">${transacoes.descricao}</td>
+        `   <td class="descricao">${transacao.descricao}</td>
             <td class=${CSSclasse}>${valor}</td>
-            <td class="data">${transacoes.data}</td>   
+            <td class="data">${transacao.data}</td>   
         `
         // Cria os tds e retorna para fora
         return html
@@ -120,19 +132,34 @@ const DOM = {
     atualizarValoresCartoes() {
         document
             .getElementById('mostraEntradas')
-            .innerHTML = uteis.formatoMoeda(transacao.entradas())
+            .innerHTML = uteis.formatoMoeda(Transacao.entradas())
     
         document
             .getElementById('mostraSaidas')
-            .innerHTML = uteis.formatoMoeda(transacao.saidas())
+            .innerHTML = uteis.formatoMoeda(Transacao.saidas())
     
         document
             .getElementById('mostraTotal')
-            .innerHTML = uteis.formatoMoeda(transacao.total())
+            .innerHTML = uteis.formatoMoeda(Transacao.total())
+    },
+
+    limparTransacoes() {
+        DOM.containerTransacoes.innerHTML = ""
     }
 }
 
 const uteis = {
+    formatarValor(valor) {
+      valor = Number(valor) * 100
+      return valor
+    },
+
+    formatarData(data) {
+        const separarData = data.split("-")
+        return `${separarData[2]}/${separarData[1]}/${separarData[0]}`
+    },
+
+
     formatoMoeda(valor) {
         // Força o valor para ser number
         const sinal = Number(valor) < 0 ? "-" : ""
@@ -152,14 +179,100 @@ const uteis = {
     }
 }
 
+const form = {
+    descricao: document.querySelector('input#descricao'),
+    valor: document.querySelector('input#valor'),
+    data: document.querySelector('input#data'),
+
+    pegarValores() {
+        return {
+            descricao: form.descricao.value,
+            valor: form.valor.value,
+            data: form.data.value
+        }
+    },
+
+    validarCampos() {
+        const { descricao, valor, data } = form.pegarValores()
+
+        if( descricao.trim() === "" || 
+            valor.trim() === "" || 
+            data.trim() === "") {
+                throw new Error("Por favor, preencha todos os campos")
+        }
+    },
+
+    
+    formatarValores() {
+        let { descricao, valor, data } = form.pegarValores()
+
+        valor = uteis.formatarValor(valor)
+
+        data = uteis.formatarData(data)
+
+        return {
+            descricao,
+            valor,
+            data
+        }
+    },
+
+    limparCampos() {
+        form.descricao.value = ""
+        form.valor.value = ""
+        form.data.value = ""
+    },
+
+
+    submit(event) {
+        event.preventDefault()
+
+        try {
+            form.validarCampos()
+            const transacao = form.formatarValores()
+            Transacao.add(transacao)
+            form.limparCampos()
+            caixaFormulario.fecharFormulario()
+
+        } catch (error) {
+            alert(error.message)
+        }
+
+    }
+}
+
+const app = {
+    iniciar() {
+
+        // Para cada obj, cria o html em cada posição do obj transacoes
+        Transacao.all.forEach(function(transacao) {
+            DOM.adicionarTransacao(transacao)
+        })
+
+        DOM.atualizarValoresCartoes()
+        
+
+    },
+    reiniciar() {
+        DOM.limparTransacoes()
+        app.iniciar()
+
+    },
+}
+
+app.iniciar()
+
+// transacao.add({
+//     // id: 39,
+//     descricao: 'alo',
+//     valor: 200,
+//     data: '18/04/2022'
+// })
+
 // Chamar a funcão adicionarTransacao pegando a posição 0 do obj transacoes e jogando pra dentro da func adicionarTransacao
 // DOM.adicionarTransacao(transacoes[0])
 // DOM.adicionarTransacao(transacoes[1])
 // DOM.adicionarTransacao(transacoes[2])
 
-// Para cada obj, cria o html em cada posição do obj transacoes
-transacoes.forEach(function(transacoes) {
-    DOM.adicionarTransacao(transacoes)
-})
 
-DOM.atualizarValoresCartoes()
+
